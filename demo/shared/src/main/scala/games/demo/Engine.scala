f8c127918b52780f25d8e5e6da85fbc72ccb41d9
Joël Rossier <joel.rossier@outlook.com>
2015-04-26 12:55:16 +0200

    Corrected speed

--- demo/shared/src/main/scala/games/demo/Engine.scala
+++ demo/shared/src/main/scala/games/demo/Engine.scala
@@ -47,8 +47,8 @@ class Engine(itf: EngineInterface)(implicit ec: ExecutionContext) extends games.
   val hitDamage: Float = 25f
   val terrainSize: Float = 50f
   val initHealth: Float = 100f
-  val minSpeed: Float = 0.1f
-  val maxSpeed: Float = 0.3f
+  val minSpeed: Float = 2f
+  val maxSpeed: Float = 6f
   val maxDeviceAngle: Float = 90f
 
   def context: games.opengl.GLES2 = gl
