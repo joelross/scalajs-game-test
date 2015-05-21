ce5679b68ec3b564caac68221a5523ed8f6a86cc
Joël Rossier <joel.rossier@outlook.com>
2015-05-21 18:36:34 +0200

    Corrected invuln time from 5s to 10s

--- demo/shared/src/main/scala/games/demo/Engine.scala
+++ demo/shared/src/main/scala/games/demo/Engine.scala
@@ -43,7 +43,7 @@ class Projectile(val id: Int, var position: Vector2f, val orientation: Float)
 class Engine(itf: EngineInterface)(implicit ec: ExecutionContext) extends games.FrameListener {
   final val updateIntervalMs: Int = 50 // Resend position at 20Hz
   final val shotIntervalMs: Int = 250 // 4 shots per second
-  final val invulnerabilityTimeMs: Int = 5000 // 5 seconds of invulnerability when spawning
+  final val invulnerabilityTimeMs: Int = 10000 // 10 seconds of invulnerability when spawning
   final val configFile: String = "/games/demo/config"
   final val initialHealth: Float = 100
   final val damagePerShot: Float = 12.5f // 8 shots to destroy
