c71a79b2df7a0ccfa18310ea15d5a29fd0b87a17
Joël Rossier <joel.rossier@outlook.com>
2015-05-27 16:54:52 +0200

    Changed colors

--- demo/shared/src/main/scala/games/demo/Data.scala
+++ demo/shared/src/main/scala/games/demo/Data.scala
@@ -6,13 +6,14 @@ import scala.collection.immutable
 import scala.collection.mutable
 
 object Data {
-  val colors: immutable.Map[Int, Vector3f] = immutable.Map(1 -> new Vector3f(1f, 0f, 0f), // Red for player 1
+  val colors: immutable.Map[Int, Vector3f] = immutable.Map(
+    1 -> new Vector3f(1f, 0f, 0f), // Red for player 1
     2 -> new Vector3f(0f, 0f, 1f), // Blue for player 2
     3 -> new Vector3f(0f, 1f, 0f), // Green for player 3
     4 -> new Vector3f(1f, 1f, 0f), // Yellow for player 4
     5 -> new Vector3f(0f, 1f, 1f), // Cyan for player 5
     6 -> new Vector3f(1f, 0.6f, 0.2f), // Orange for player 6
     7 -> new Vector3f(0.8f, 0f, 0.8f), // Purple for player 7
-    8 -> new Vector3f(0.6f, 0.3f, 0f) // Brown for player 8
+    8 -> new Vector3f(1f, 0f, 0.5f) // Pink for player 8
     )
 }
